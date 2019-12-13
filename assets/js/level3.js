3c17cd8a993b6c0b9e39c13ddffee7626e249062
ecl5pse <fantamas20@gmail.com>
2019-12-13 21:39:19 +0200

    start level 3 basic seturn 	add  style css

---
+++ assets/js/level3.js
@@ -0,0 +1,18 @@
+'use strict';
+
+class PrintMachine {
+    constructor() {
+        this.sizeFont = sizeFont;
+        this.colorFont = colorFont;
+        this.familiesFont = familiesFont;
+
+    }
+
+    print() {
+
+
+
+    }
+
+
+}
